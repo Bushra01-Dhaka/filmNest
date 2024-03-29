4fd8fa357dfc6512a970a8d1b935d4a01db374fc
Bushra01-Dhaka <humayraanjum87@gmail.com>
2024-03-29 18:57:07 +0600

    Created a ShowMovies component

---
+++ src/Component/NormalCompo/Shows/ShowMovies.jsx
@@ -0,0 +1,11 @@
+
+
+const ShowMovies = () => {
+    return (
+        <div className="min-h-[100vh] py-20 bg-[#FE0000]">
+            <h1 className="text-center text-6xl uppercase text-white font-bold">Streaming now</h1>
+        </div>
+    );
+};
+
+export default ShowMovies;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 
 import Banner from "../../Component/NormalCompo/Banner";
 import Featured from "../../Component/NormalCompo/Featured";
+import ShowMovies from "../../Component/NormalCompo/Shows/ShowMovies";
 const Home = () => {
     return (
         <div>
            <Banner></Banner>
            <Featured></Featured>
+           <ShowMovies></ShowMovies>
         </div>
     );
 };
